ddf91caa127fbd00ca48b6ccd66eb37c81476544
sarathsanjay344@gmail.com <sarathsanjay344@gmail.com>
2025-03-13 10:20:45 +0530

    name

---
+++ ex1.java
@@ -0,0 +1,12 @@
+public class ex1{
+    public static void main(String[]args){
+        String name="sarath";
+        System.out.println("enter the number:"+name);
+        String sub="cse";
+        System.out.println("enter oyur subject:"+sub);
+        int mark=06;
+        System.out.println("enter your mark:"+mark);
+        String grade="u";
+        System.out.println("enter your grade:"+grade);
+    }
+}
