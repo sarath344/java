9fcbcb7654f097ad7944e50e786d5cb17445419a
sarathsanjay344@gmail.com <sarathsanjay344@gmail.com>
2025-03-15 11:43:17 +0530

    coffee

---
+++ cof.java
@@ -0,0 +1,18 @@
+ public class cof{
+    public static void main(String[]args){
+        int cof=2;
+        switch(cof){
+            case 1:System.out.println("black cof");
+            break;
+            case 2:System.out.println("capuchino cof");
+            break;
+            case 3:System.out.println("white cof");
+            break;
+            case 4:System.out.println("cool cof");
+            break;
+            default:
+                System.out.println("the cof is not available");
+        
+        }
+    }
+ }
